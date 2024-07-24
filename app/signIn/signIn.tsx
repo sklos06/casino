1f84f203d968921ff4ebd8abf40c9ea1db77f0d4
Szymon <sklos0620@gmail.com>
2024-07-24 11:14:48 +0200

    handleInputs

--- app/signIn/signIn.tsx
+++ app/signIn/signIn.tsx
@@ -1,7 +1,27 @@
-import React from 'react';
+'use client'
+import React, {useState} from 'react';
 import styles from './signIn.module.css'
 
+type LogIn = {
+    username: string,
+    password: string
+}
+
 function SignIn() {
+    const [user, setUser] = useState<LogIn>({
+            username: '',
+            password: ''
+        })
+
+    function handleInput(event: React.ChangeEvent<HTMLInputElement>): void {
+        const {name, value} = event.target
+        setUser({
+            ...user,
+            [name]: value
+        })
+    }
+
+
     return (
         <section className={styles.page}>
             <form action="POST" className={styles.form}>
@@ -9,17 +29,15 @@ function SignIn() {
                     <label htmlFor="username">Username:
                         <span className={styles.required}>*</span>
                     </label>
-                    <input type="text" id="username"/>
+                    <input type="text" id="username" name="username" value={user.username} onChange={handleInput}/>
                 </div>
                 <div className={styles.formGroup}>
                     <label htmlFor="password">Password:
                         <span className={styles.required}>*</span>
                     </label>
-                    <input type="text" id="password"/>
+                    <input type="password" id="password" name="password" value={user.password} onChange={handleInput}/>
                 </div>
-                <div className={styles.formGroup}>
-                    <input type="button" value="Sign in" className={styles.btn}/>
-                </div>
+                <input type="submit" value="Sign in" className={styles.btn}/>
             </form>
         </section>
     );
--- app/signUp/signUp.tsx
+++ app/signUp/signUp.tsx
@@ -1,7 +1,43 @@
-import React from 'react';
+'use client'
+import React, {useState} from 'react';
 import styles from './signUp.module.css'
 
+type User = {
+    username: string,
+    password: string,
+    repassword: string,
+    email: string,
+    firstName: string,
+    lastName: string,
+    phone: string,
+    country: string,
+    city: string,
+    address: string,
+}
+
 function SignUp() {
+
+    const [user, setUser] = useState<User>({
+        username: '',
+        password: '',
+        repassword: '',
+        email: '',
+        firstName: '',
+        lastName: '',
+        phone: '',
+        country: '',
+        city: '',
+        address: '',
+    });
+
+    function handleInput(event: React.ChangeEvent<HTMLInputElement>): void {
+        const {name, value} = event.target
+        setUser({
+            ...user,
+            [name]: value
+        })
+    }
+
     return (
         <section className={styles.page}>
             <form action="POST" className={styles.form}>
@@ -11,25 +47,28 @@ function SignUp() {
                             <label htmlFor="email">E-mail:
                                 <span className={styles.required}>*</span>
                             </label>
-                            <input type="text" id="email"/>
+                            <input type="text" id="email" name="email" value={user.email} onChange={handleInput}/>
                         </div>
                         <div className={styles.formGroup}>
                             <label htmlFor="username">Username:
                                 <span className={styles.required}>*</span>
                             </label>
-                            <input type="text" id="username"/>
+                            <input type="text" id="username" name="username" value={user.username}
+                                   onChange={handleInput}/>
                         </div>
                         <div className={styles.formGroup}>
                             <label htmlFor="password">Password:
                                 <span className={styles.required}>*</span>
                             </label>
-                            <input type="text" id="password"/>
+                            <input type="text" id="password" name="password" value={user.password}
+                                   onChange={handleInput}/>
                         </div>
                         <div className={styles.formGroup}>
                             <label htmlFor="repassword">Repeat password:
                                 <span className={styles.required}>*</span>
                             </label>
-                            <input type="text" id="repassword"/>
+                            <input type="text" id="repassword" name="repassword" value={user.repassword}
+                                   onChange={handleInput}/>
                         </div>
                     </div>
                     <div className={styles.rightSide}>
@@ -37,33 +76,35 @@ function SignUp() {
                             <label htmlFor="firstName">First name:
                                 <span className={styles.required}>*</span>
                             </label>
-                            <input type="text" id="firstName"/>
+                            <input type="text" id="firstName" name="firstName" value={user.firstName}
+                                   onChange={handleInput}/>
                         </div>
                         <div className={styles.formGroup}>
                             <label htmlFor="lastName">Last name:
                                 <span className={styles.required}>*</span>
                             </label>
-                            <input type="text" id="lastName"/>
+                            <input type="text" id="lastName" name="lastName" value={user.lastName}
+                                   onChange={handleInput}/>
                         </div>
                         <div className={styles.formGroup}>
                             <label htmlFor="phone">Phone:</label>
-                            <input type="text" id="phone"/>
+                            <input type="text" id="phone" name="phone" value={user.phone} onChange={handleInput}/>
                         </div>
                         <div className={styles.formGroup}>
                             <label htmlFor="country">Country:</label>
-                            <input type="text" id="country"/>
+                            <input type="text" id="country" name="country" value={user.country} onChange={handleInput}/>
                         </div>
                         <div className={styles.formGroup}>
                             <label htmlFor="city">City:</label>
-                            <input type="text" id="city"/>
+                            <input type="text" id="city" name="city" value={user.city} onChange={handleInput}/>
                         </div>
                         <div className={styles.formGroup}>
                             <label htmlFor="address">Address:</label>
-                            <input type="text" id="address"/>
+                            <input type="text" id="address" name="address" value={user.address} onChange={handleInput}/>
                         </div>
                     </div>
                 </div>
-                <input type="button" value="Sign up" className={styles.btn}/>
+                <input type="submit" value="Sign up" className={styles.btn}/>
             </form>
         </section>
 
