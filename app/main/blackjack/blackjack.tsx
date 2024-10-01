7d24c3fe71e54605a34dbe8ad0b5918cca96187f
Szymon <sklos0620@gmail.com>
2024-10-01 18:09:45 +0200

    betting player's money, deleting from database, and posting changes back to base

--- app/main/blackjack/blackjack.tsx
+++ app/main/blackjack/blackjack.tsx
@@ -1,25 +1,39 @@
 'use client'
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import styles from './blackjack.module.css'
-import {func} from "prop-types";
 
+// Define the Blackjack component
 function Blackjack() {
 
+    // Type definitions for card and hand
     type Card = {
-        img: string;
-        value: number;
-    }
+        img: string;   // Image path of the card
+        value: number; // Value of the card in blackjack
+    };
+
     type Hand = {
-        cards: Card[],
-        totalValue: number
-    }
+        cards: Card[];      // Array of card objects in the hand
+        totalValue: number; // Total value of the cards in hand
+    };
 
     interface Player {
-        username: string,
-        money: number
+        username: string;   // Player's username
+        money: number;      // Player's available money
     }
 
-    const [text, setText] = useState<string>("PLAY");
+    // Initialize state variables
+    const [text, setText] = useState<string>("PLAY"); // Button text for "PLAY"/"PLAY AGAIN"
+    const [deck, setDeck] = useState<Card[]>([]); // Deck of cards
+    const [playerHand, setPlayerHand] = useState<Hand>({cards: [], totalValue: 0}); // Player's hand
+    const [croupierHand, setCroupierHand] = useState<Hand>({cards: [], totalValue: 0}); // Dealer's hand
+    const [initialized, setInitialized] = useState<boolean>(false); // Game initialization flag
+    const [isStopped, setIsStopped] = useState<boolean>(false); // Game stop flag
+    const [startGame, setStartGame] = useState<boolean>(false); // Start game flag
+    const [player, setPlayer] = useState<Player>({username: "Guest", money: 0}); // Player data
+    const [bet, setBet] = useState<number>(); // Bet amount
+    const [newMoney, setNewMoney] = useState<number>(0); // Value of player's money after changes
+
+    // Basic deck of cards (standard 52-card deck)
     const basicDeck: Card[] = [
         {img: 'ace-club.png', value: 11},
         {img: 'ace-diamond.png', value: 11},
@@ -74,138 +88,113 @@ function Blackjack() {
         {img: 'two-heart.png', value: 2},
         {img: 'two-spade.png', value: 2}
     ];
-    const [deck, setDeck] = useState<Card[]>([]);
-    const [playerHand, setPlayerHand] = useState<Hand>({
-        cards: [],
-        totalValue: 0
-    })
-    const [croupierHand, setCroupierHand] = useState<Hand>({
-        cards: [],
-        totalValue: 0
-    })
-    const [initialized, setInitialized] = useState<boolean>(false);
-    const [isStopped, setIsStoppped] = useState<boolean>(false);
-    const [startGame, setStartGame] = useState<boolean>(false);
-    const [player, setPlayer] = useState<Player>({
-        username: "Guest",
-        money: 0
-    })
 
+    // Fetch player data when the component loads
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await fetch('/api/playerData', {
-                    method: 'GET',
-                    credentials: 'include',
-                });
-
+                const response = await fetch('/api/playerData', {method: 'GET', credentials: 'include'});
                 if (response.ok) {
                     const data = await response.json();
-                    setPlayer({
-                        money: data.money,
-                        username: data.username,
-                    });
+                    setPlayer({money: data.money, username: data.username});
                 } else {
                     const errorData = await response.json();
-                    console.log(`Błąd: ${errorData.error}`);
+                    console.log(`Error: ${errorData.error}`);
                 }
             } catch (error) {
-                console.log('Wystąpił błąd podczas pobierania punktów.');
+                console.log('Error fetching player data.');
             }
         };
         fetchData();
-
     }, []);
+
+
+    // Start the game by resetting hands and deck when the "startGame" flag changes
     useEffect(() => {
         if (startGame) {
-            setDeck(basicDeck);
-            setPlayerHand({
-                cards: [],
-                totalValue: 0
-            });
-            setCroupierHand({
-                cards: [],
-                totalValue: 0
-            })
-            setIsStoppped(false);
-            setInitialized(true);
-            setStartGame(false);
+            setDeck(basicDeck); // Reset deck
+            setPlayerHand({cards: [], totalValue: 0}); // Reset player hand
+            setCroupierHand({cards: [], totalValue: 0}); // Reset dealer hand
+            setIsStopped(false); // Reset stopped state
+            setInitialized(true); // Set initialized to true
+            setStartGame(false); // Reset start game flag
         }
-    }, [startGame])
+    }, [startGame]);
 
+    // Function to get a random card from the deck
     function getRandomCard<T>(cards: T[]): T {
         const randomIndex: number = Math.floor(Math.random() * cards.length);
         return cards[randomIndex];
     }
 
+    // Function to handle player "HIT" action
     function handleHit(): void {
-        if (deck.length > 0 && !(isStopped)) {
-            const newCard: Card = getRandomCard(deck);
-            setPlayerHand(p => {
-                const newTotalValue: number = p.totalValue + newCard.value;
-                return {
-                    cards: [...p.cards, newCard],
-                    totalValue: newTotalValue
-                };
+        if (deck.length > 0 && !isStopped) {
+            const newCard: Card = getRandomCard(deck); // Get a random card from the deck
+            setPlayerHand((prevHand) => {
+                const newTotalValue = prevHand.totalValue + newCard.value;
+                return {cards: [...prevHand.cards, newCard], totalValue: newTotalValue};
             });
-            setDeck(d => d.filter(card => card !== newCard));
+            setDeck((prevDeck) => prevDeck.filter(card => card !== newCard)); // Remove card from deck
         }
     }
 
 
+    // Deal initial cards to the dealer and player when game is initialized
     useEffect(() => {
         if (initialized) {
             if (deck.length > 0) {
+                // Give dealer an initial card
                 const initialCard: Card = getRandomCard(deck);
-                setCroupierHand(c => {
-                    const newTotalValue: number = c.totalValue + initialCard.value;
-                    return {
-                        cards: [initialCard],
-                        totalValue: newTotalValue
-                    };
+                setCroupierHand((prevHand) => {
+                    const newTotalValue = prevHand.totalValue + initialCard.value;
+                    return {cards: [initialCard], totalValue: newTotalValue};
                 });
-                setDeck(d => d.filter(card => card !== initialCard));
+                setDeck((prevDeck) => prevDeck.filter(card => card !== initialCard));
             }
-            handleHit()
-            handleHit()
-            setInitialized(false);
+            handleHit(); // Give player two initial cards
+            handleHit();
+            setInitialized(false); // Reset initialization flag
         }
     }, [initialized]);
 
+
+    // Croupier (dealer) turn logic
     function croupierTurn(): void {
         if (deck.length > 0 && croupierHand.totalValue <= playerHand.totalValue && croupierHand.totalValue < 17) {
             const newCard: Card = getRandomCard(deck);
-            setCroupierHand(c => {
-                const newTotalValue: number = c.totalValue + newCard.value;
-                return {
-                    cards: [...c.cards, newCard],
-                    totalValue: newTotalValue
-                };
+            setCroupierHand((prevHand) => {
+                const newTotalValue = prevHand.totalValue + newCard.value;
+                return {cards: [...prevHand.cards, newCard], totalValue: newTotalValue};
             });
-            setDeck(d => d.filter(card => card !== newCard));
+            setDeck((prevDeck) => prevDeck.filter(card => card !== newCard));
         }
     }
 
+    // Handle player "STAND" action
     function handleStand(): void {
-        setIsStoppped(true);
-        if (!(isStopped)) croupierTurn();
+        setIsStopped(true); // Stop player actions
+        if (!isStopped) croupierTurn(); // Dealer's turn
     }
 
+    // Monitor player's hand for bust or blackjack
     useEffect(() => {
         if (playerHand.totalValue > 21) {
-            setIsStoppped(true);
+            setIsStopped(true);
             console.log("YOU LOST");
         } else if (playerHand.totalValue === 21) {
             console.log("BLACKJACK");
-            setIsStoppped(true);
-            croupierTurn();
+            setIsStopped(true);
+            croupierTurn(); // Dealer's turn on player blackjack
         }
     }, [playerHand]);
 
+    // Check the result based on dealer's hand and player's hand
     useEffect(() => {
         if (croupierHand.cards.length > 1 && croupierHand.totalValue < playerHand.totalValue && croupierHand.totalValue < 17) {
-            setTimeout(croupierTurn, 1000);
+            setTimeout(croupierTurn, 1000); // Dealer hits again
         }
+        // Win/Loss/Draw logic
         if (croupierHand.totalValue > playerHand.totalValue && croupierHand.totalValue <= 21) {
             console.log("YOU LOST!");
         } else if (croupierHand.totalValue === playerHand.totalValue && croupierHand.totalValue === 21) {
@@ -215,15 +204,64 @@ function Blackjack() {
         }
     }, [croupierHand]);
 
+    // Start a new game when the player clicks "PLAY"
     function handlePlay(): void {
-        setStartGame(true);
-        setText("PLAY AGAIN");
+        setStartGame(true); // Set game start flag
+        setText("PLAY AGAIN"); // Update button text
     }
 
+    // Handle bet input change
+    function handleBetInput(event: React.ChangeEvent<HTMLInputElement>): void {
+        const stake: number = Number(event.target.value);
+        setBet(stake); // Set the bet amount
+    }
+
+
+    // Place a bet and deduct money from player's balance
+    function handleBet(): void {
+        if (bet) {
+            if (bet > player.money) {
+                console.log("You can't bet more than you have");
+            } else {
+                setNewMoney(player.money - bet); // Deduct bet from player's money
+            }
+        }
+    }
+
+    // Update player's money on the server
+    const updateMoney = async () => {
+        try {
+            const response = await fetch('/api/playerData', {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({username: player.username, money: newMoney})
+            });
+            if (response.ok) {
+                console.log('Successfully updated user balance');
+            } else {
+                const errorData = await response.json();
+                console.log(`Error: ${errorData.error}`);
+            }
+        } catch (error) {
+            console.log(`Error updating player balance. ${error}`);
+        }
+    };
+    // Trigger update money when player's money changes
+    useEffect(() => {
+        if (newMoney !== 0) {
+            setPlayer((prevPlayer) => {
+                return {...prevPlayer, money: newMoney};
+            });
+            updateMoney();
+        }
+    }, [newMoney]);
+
+
+    // JSX rendering the game UI
     return (
         <div className={styles.blackjackPage}>
-            <header>
-                <p>{player.username}</p>
+            <header className={styles.userData}>
+                <p>User: {player.username}</p>
                 <p>Your money: {player.money}</p>
             </header>
             <main>
@@ -264,6 +302,10 @@ function Blackjack() {
                     <button className={styles.btn} onClick={handleStand}>STAND</button>
                     <button className={styles.btn}>DOUBLE DOWN</button>
                     <button className={styles.btn} onClick={handlePlay}>{text}</button>
+                </div>
+                <div className={styles.buttons}>
+                    <input type="number" value={bet} onChange={handleBetInput} placeholder="0"/>
+                    <button className={styles.btn} onClick={handleBet}>BET</button>
                 </div>
             </main>
         </div>
--- pages/api/playerData.ts
+++ pages/api/playerData.ts
@@ -1,18 +1,41 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import { authenticate } from '../../middleware/auth';
+import {NextApiRequest, NextApiResponse} from 'next';
+import {authenticate} from '../../middleware/auth';
+import User from '../../models/User';
+
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    const user = await authenticate(req, res);
 
-    if (!user) {
-        return;
-    }
 
-    try {
-        const money:number = user.money;
-        const username: string = user.username;
-        res.status(200).json({ username,money });
-    } catch (error) {
-        res.status(500).json({ error: 'Wystąpił błąd podczas pobierania punktów użytkownika.' });
+    if (req.method === 'GET') {
+        const user = await authenticate(req, res);
+        if (!user) {
+            return;
+        }
+        try {
+            const money: number = user.money;
+            const username: string = user.username;
+            res.status(200).json({username, money});
+        } catch (error) {
+            res.status(500).json({error: 'Wystąpił błąd podczas pobierania punktów użytkownika.'});
+        }
+    } else if (req.method === 'POST') {
+        const { username, money } = req.body;
+
+        try {
+
+            const [updatedCount] = await User.update(
+                { money },
+                { where: { username } }
+            );
+
+            if (updatedCount === 0) {
+                return res.status(404).json({ error: 'Nie znaleziono użytkownika do aktualizacji.' });
+            } else {
+                res.status(200).json({ success: true, message: 'Pieniądze zostały zaktualizowane.' });
+            }
+        } catch (error) {
+            console.error('Błąd przy aktualizacji pieniędzy:', error);
+            res.status(500).json({ error: 'Wystąpił błąd podczas aktualizacji pieniędzy.' });
+        }
     }
 }
